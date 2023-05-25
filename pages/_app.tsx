df1e80a943bd7f0f3cec8602be9c00f2be78fb37
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2023-05-25 17:29:05 +0530

    initial setup done

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css';
+import '../styles/Home.scss';
+import '../styles/Toaster.scss';
+import type {AppProps} from 'next/app';
+import React, {useEffect} from 'react';
+import Toaster from '../src/components/Toaster';
+
+function MyApp({Component, pageProps}: AppProps) {
+	return (
+		<React.Fragment>
+			<Toaster />
+			<Component {...pageProps} />
+		</React.Fragment>
+	);
+}
+
+export default MyApp;
