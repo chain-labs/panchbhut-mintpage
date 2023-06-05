3b71b72222ac56fdafa7b003756078ea8b94bf97
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2023-06-05 19:41:26 +0530

    added contracts and ethers

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
+import useCustomContract from '../../ethereum/useCustomContract';
+import {useProvider} from 'wagmi';
+import {ethers} from 'ethers';
+import {CONTRACT_ADDRESS} from '../../utils/constants';
 
 const HomeContainer = () => {
+	const provider = useProvider();
+	const [contract, setContract] = useState<ethers.Contract>();
+	const contractAddress = CONTRACT_ADDRESS;
+
 	return <div>HomeContainer</div>;
 };
 
---
+++ src/redux/store.ts
@@ -0,0 +1,28 @@
+import { configureStore, Action, ThunkAction } from '@reduxjs/toolkit'
+import { createWrapper } from 'next-redux-wrapper'
+// import { networkReducer } from './network'
+import { userReducer } from './user/reducers'
+
+const makeStore = () =>
+  configureStore({
+    reducer: {
+      user: userReducer,
+    //   network: networkReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({ serializableCheck: false }),
+    devTools: true,
+  })
+
+export type AppStore = ReturnType<typeof makeStore>
+
+export type AppState = ReturnType<AppStore['getState']>
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppState,
+  unknown,
+  Action<string>
+>
+
+export const wrapper = createWrapper<AppStore>(makeStore)
