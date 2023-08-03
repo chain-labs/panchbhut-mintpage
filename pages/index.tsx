96e6d4933e1afc19bb5314836abff4c3d8b6150b
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2023-08-04 03:57:41 +0530

    removed next page in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import React from 'react';
 import HomeContainer from '../src/containers/home';
 
-const Home: NextPage = () => {
+const Home = () => {
 	return (
 		<React.Fragment>
 			<HomeContainer />
