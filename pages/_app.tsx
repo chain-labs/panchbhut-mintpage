8bc14d7e73b851b3c64b5644d7b26ddea6e7008a
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2023-08-04 03:46:24 +0530

    resolving vercel error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,22 @@ import Wagmi from 'src/components/Wagmi';
 import Navbar from 'src/components/Navbar';
 import {wrapper} from 'src/redux/store';
 import 'src/styles/global.css';
+import Head from 'next/head';
 
 function MyApp({Component, pageProps}: AppProps) {
 	return (
 		<Wagmi>
+			<Head>
+				<title>Panchbut Game | Mint Here</title>
+				<link
+					rel="shortcut icon"
+					href=" https://pranapsivadasan.github.io/panchbhut/assets/navLogo.png"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+			</Head>
 			{/* <Navbar /> */}
 			<Component {...pageProps} />
 		</Wagmi>
