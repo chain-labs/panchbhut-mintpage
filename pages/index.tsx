377feef17bc1b12bd26f0abea4063a66f4fcb93e
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2023-09-27 21:08:13 +0530

    removed build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type {NextPage} from 'next';
 import Head from 'next/head';
 import React from 'react';
 import HomeContainer from '../src/containers/home';
-import Test from 'src/containers/home/Test';
 
 const Home: NextPage = () => {
 	return (
